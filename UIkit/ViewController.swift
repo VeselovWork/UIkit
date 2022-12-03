7022553ab0fac8b125f3bd37f32a71313576f842
AW <veselovwork@gmail.com>
2022-12-03 18:02:27 +0300

    Initial Commit

---
+++ UIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIkit
+//
+//  Created by Andrey on 03.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
